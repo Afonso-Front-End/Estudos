4240a84d14faa6982bc1d361348e45e593984e46
Afonso <129227018+Afonso-Front-End@users.noreply.github.com>
2023-07-12 13:12:34 -0300

    Atualizando

---
+++ apex_front_end/reactjs/04-importar-componentes/src/Componete01.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// Cria componenete
+class Componente01 extends React.Component{
+    render(){
+        return(
+            <h1>Esse é o meu primeiro componenete</h1>
+        )
+    }
+}
+
+// Exportar
+
+export default Componente01
---
+++ apex_front_end/reactjs/04-importar-componentes/src/Componete02.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Cria componenete
+class Componente02 extends React.Component{
+    render(){
+        return(
+            <p>Esse é o meu segundo componenete</p>
+        )
+    }
+}
+
+// Exportar
+export default Componente02
