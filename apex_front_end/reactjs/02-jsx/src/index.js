633e1c8879cd8760b50179f23b1df493a62467c1
Afonso <129227018+Afonso-Front-End@users.noreply.github.com>
2023-07-12 12:56:15 -0300

    Atualizando

---
+++ apex_front_end/reactjs/02-jsx/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import ReactDOM from 'react-dom'
+
+// Sem JSX
+const elemento = React.createElement('h1',{},'Sem JSX')
+ReactDOM.render(
+    elemento,
+    document.getElementById('root')
+)
+
+// COm JSX
+ReactDOM.render(
+    <h1>Com JSX</h1>,
+    document.getElementById('root')
+)
