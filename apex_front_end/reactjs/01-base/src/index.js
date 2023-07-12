8ff0db18716ee93e1fa77421bbc875b3a16b8335
Afonso <129227018+Afonso-Front-End@users.noreply.github.com>
2023-07-12 12:37:09 -0300

    Atualizando

---
+++ apex_front_end/reactjs/01-base/src/index.js
@@ -0,0 +1,18 @@
+// Importar modulo
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+// Estrutura
+const estrutura = 
+<div>
+    <h1>Meu primeiro render</h1>,
+    <p>Testando...</p>
+</div>
+
+
+// Importar rendrn
+ReactDOM.render(
+    estrutura,
+    document.getElementById('root')
+);
